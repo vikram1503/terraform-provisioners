0bc42bc11053155e021a2df865b2c098f5040d0c
vikram1503 <vikram15031989@gmail.com>
2024-05-14 07:12:29 +0530

    practice

---
+++ ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "db" {
+
+    ami = "ami-090252cbe067a9e58"
+    vpc_security_group_ids = ["sg-07832342d5cdd6bb4"]
+    instance_type = "t2.micro"
+
+    provisioner "local-exec" {
+        command = "echo ${self.private_ip} > private_ips.txt"
+    }
+
+     provisioner "local-exec" {
+        command = "ansible-playbook -i private_ips.txt web.yaml"
+    }
+
+    connection {
+        type     = "ssh"
+        user     = "ec2-user"
+        password = "Devops321"
+        host     = self.public_ip
+    }
+
+      provisioner "remote-exec" {
+      inline = [
+        "sudo dnf install ansible -y",
+        "sudo dnf install nginx -y",
+        "sudo systemctl start nginx"
+
+    ]
+  }
+}
+
+  
+} 
